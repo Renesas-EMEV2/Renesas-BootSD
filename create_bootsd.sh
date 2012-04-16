6f55ffaf84502e764962293113f6cb2cc0767de6
ffxx68 <ffumi68@gmail.com>
2012-04-16 14:37:21 +0200

    Revised procedure

--- create_bootsd.sh
+++ create_bootsd.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 ############################################
-# Script to create a bootable sd card for  ~
-# the Renesas tablet with                  ~
-#                                          ~
-#        999 < kernel version < 10.000     ~
-#                                          ~
-# v20110819                                ~
-#### sd partitions #########################
+# Script to create a bootable sd card for
+# the Renesas EMEV tablet
+#
+# Assuming these partitions exist:
+# (Use part_sd.sh to create them)
 #
 # p1 500MB	: 	boot files
 # p2 256KB	:	uboot environment
@@ -15,27 +13,29 @@
 # p5 750MB	:	data-fs / cache
 # p6 the rest	:	nand-fs
 #
-# 
-############################################
-# tosan                                    ~ 
 ############################################
 
 set +x -e
 
 SDCARD=$1
 
-SDBOOT="sdboot.bin"
-UBOOTSD="uboot-sd.bin"
-PATCHED_UBOOT="uboot-sd.bin-PATCHED"
+#UBOOT="/media/u01/RenesasEV2/bootloader/u-boot"
+UBOOTDIR="."
+AOSPDIR=$2
+KERNELDIR=$2
+
+UIMAGE="${KERNELDIR}/uImage4"
+UIMAGENAME="uImage4"
+SDBOOT="${UBOOTDIR}/sdboot.bin"
+SDBOOTNAME="sdboot.bin"
+PATCHED_UBOOT="${UBOOTDIR}/uboot-sd.bin-PATCHED"
+UBOOTSDNAME="uboot-sd.bin"
+ANDROIDFS="${AOSPDIR}/android-fs4.tar.gz"
 INITRC="init.rc"
 VOLDSTAB="vold.fstab"
 VOLDCONF="vold.conf"
-UIMAGE="../uImage4"
-ANDROIDFS="../android-fs4.tar.gz"
 
-echo .
 echo "(1) Checking file availabilities ..."
-echo .
 ls -oh $SDBOOT
 ls -oh $PATCHED_UBOOT
 ls -oh $INITRC
@@ -44,111 +44,31 @@ ls -oh $VOLDCONF
 ls -oh $UIMAGE
 ls -oh $ANDROIDFS
 
+mkdir -p _tmp_/p1
+mkdir -p _tmp_/p3
+mount -t vfat  ${SDCARD}1 ./_tmp_/p1/
+mount -t ext3   ${SDCARD}3 ./_tmp_/p3/
 
-if [ ! -b "$SDCARD" ]
-then
-    echo .
-    echo "'${SDCARD}' is not a block device!"
-    echo .
-    exit -1
-fi
+echo "(2) Copying boot files"
+cp -vf $SDBOOT ./_tmp_/p1/$SDBOOTNAME
+cp -vf $UIMAGE ./_tmp_/p1/$UIMAGENAME
+# Swapping SD u-boot with the one patched for root fs to SD partition 3
+cp -vf $PATCHED_UBOOT  ./_tmp_/p1/$UBOOTSDNAME
 
-echo .
-echo "(2) You're about to erase all data on '${SDCARD}/' !"
-read -p "If you're really sure about that, then please hit <ENTER> or abort operation by <CTRL>-C ." 
-echo .
+echo "(3) Extracting Android fs"
+tar zxf $ANDROIDFS -C ./_tmp_/p3/
+#cp -vpf ../$INITRC ./_tmp_/p3/
+#cp -vpf ../$VOLDSTAB ../$VOLDCONF ./_tmp_/p3/system/etc/
 
-set +e
-for n in "${SDCARD}*" ;
-do umount -f $n >/dev/null 2>&1 ;
-done
-set -e
-
-echo .
-echo "(3) Partioning sd card ..."
-fdisk ${SDCARD} <<_EOF_ >/dev/null 2>&1
-o
-n
-p
-1
-
-+500M
-n
-p
-2
-
-+256K
-n
-p
-3
-
-+400M
-n
-e
-
-
-n
-
-+750M
-n
-
-
-a
-1
-t
-1
-6
-w
-_EOF_
-echo .
-sleep 3
-sync;sync;sync
-
-
-echo .
-echo "(4) Creating file systems ... will take a while!"
-mkfs.msdos       -n bootfs4    ${SDCARD}1 >/dev/null 2>&1
-mke2fs -t ext3   -L androidfs4 ${SDCARD}3 >/dev/null 2>&1
-mke2fs -t ext3   -L datafs4    ${SDCARD}5 >/dev/null 2>&1
-mke2fs -t ext3   -L nandfs4    ${SDCARD}6 >/dev/null 2>&1
-echo .
-
-mkdir -p _tmp_dir_zyx_
-cd _tmp_dir_zyx_
-mkdir -p p1
-mkdir -p p3
-
-mount -t vfat  ${SDCARD}1 ./p1/
-mount -t ext3   ${SDCARD}3 ./p3/
-echo .
-echo "(5) Copying files ..."
-cp -vf ../$SDBOOT ../$UIMAGE ./p1/
-
-# Swapping SD u-boot with main u-boot (uboot4, patched for root fs to SD partition 3)
-cp -vf ../$PATCHED_UBOOT  ./p1/$UBOOTSD
-
-echo "... extracting ${ANDROIDFS} to ${SDCARD}3. This may take some time. Be patient ..."
-tar zxf ../$ANDROIDFS -C ./p3/
-cp -vpf ../$INITRC ./p3/
-cp -vpf ../$VOLDSTAB ../$VOLDCONF ./p3/system/etc/
-echo .
-
-echo .
-echo "(6) Cleaning up ..."
+echo "(4) Cleaning up"
 set +e
 dd if=/dev/zero of=${SDCARD}2 >/dev/null 2>&1
 set -e
-sleep 10
-umount -f ./p1/
-umount -f ./p3/
-echo .
-sync;sync;sync
+sleep 3
+umount -f ./_tmp_/p1/
+umount -f ./_tmp_/p3/
+rm -rf _tmp_
 
-cd ..
-rm -rf _tmp_dir_zyx_
-
-echo .
+echo "(5) Syncing up ..."
+sync
 echo "DONE."
-echo "Put the card into the tab and boot it by pressing Vol+ && Power (recovery mode)."
-echo "Have fun! :D"
-echo .
---
+++ part_sd.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+############################################
+# Script to create the partitions
+# for a bootable sd card for 
+# the Renesas EMEV tablet
+#### sd partitions #########################
+#
+# p1 500MB	: 	boot files
+# p2 256KB	:	uboot environment
+# p3 400MB	:	android-fs
+# p4		:	EXTENDED
+# p5 750MB	:	data-fs / cache
+# p6 the rest	:	nand-fs
+#
+############################################
+
+set +x -e
+
+SDCARD=$1
+
+if [ ! -b "$SDCARD" ]
+then
+    echo "'${SDCARD}' is not a block device!"
+    exit -1
+fi
+
+echo "You're about to erase all data on '${SDCARD}/' !"
+read -p "If you're really sure about that hit <ENTER>, or abort with <CTRL>-C" 
+
+set +e
+for n in "${SDCARD}*" ;
+do umount -f $n >/dev/null 2>&1 ;
+done
+set -e
+
+echo "(1) Partioning sd card ..."
+fdisk ${SDCARD} <<_EOF_ >/dev/null 2>&1
+o
+n
+p
+1
+
++500M
+n
+p
+2
+
++256K
+n
+p
+3
+
++400M
+n
+e
+
+
+n
+
++750M
+n
+
+
+a
+1
+t
+1
+6
+w
+_EOF_
+echo .
+sleep 3
+sync;sync;sync
+
+echo "(2) Creating file systems ... will take a while!"
+mkfs.msdos       -n bootfs4    ${SDCARD}1 >/dev/null 2>&1
+mke2fs -t ext3   -L androidfs4 ${SDCARD}3 >/dev/null 2>&1
+mke2fs -t ext3   -L datafs4    ${SDCARD}5 >/dev/null 2>&1
+mke2fs -t ext3   -L nandfs4    ${SDCARD}6 >/dev/null 2>&1
+
+echo "DONE."
